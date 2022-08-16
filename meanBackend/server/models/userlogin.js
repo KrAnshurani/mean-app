296bb9a8bcd6c626b9f83dbb975f0d97872fd6a3
Anshu Rani <anshu.rani@venturepact.com>
2022-08-16 15:08:05 +0530

    backend port

---
+++ meanBackend/server/models/userlogin.js
@@ -0,0 +1,25 @@
+const { Module } = require("module");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmpassword:{
+        type:String,
+        required:true
+    }
+})
+
+const Register = new mongoose.model("Register" , userSchema);
+Module.exports = Register;
