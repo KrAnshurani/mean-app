55cf03ffb970ee84dcc474f74cade61edee67803
Anshu Rani <anshu.rani@venturepact.com>
2022-08-16 15:08:48 +0530

    eangular connected to node

--- meanBackend/server/app.js
+++ meanBackend/server/app.js
@@ -1,12 +1,36 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json())
+app.use(express.urlencoded())
+
+require("./db/connection");
+const Register = require("./models/userlogin")
+
+const port = process.env.PORT || 4000;
 
 
-app.get("/" , (req,res) =>
-{
-    res.send("hello world")
+app.use (express.static(path.join( __dirname,'./public/mean-app')));
+
+// app.set('view engine', 'html');
+app.get("/" ,(req, res) =>{
+    res.render("index.html")
 });
+
+app.get("/register", (req, res) =>{
+    res.render("register");
+})
+app.post("/register", async (req, res) =>{
+    // try{
+
+        console.log(req.body.email);
+        res.send(req.body.email);
+
+
+    // } catch (error){
+    // res.status(400).send(error);
+    // }
+})
 
 app.listen(port, () =>
 {
